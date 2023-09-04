7c73d4364ed0efbdb720cb9e3c2a5577f4b0b8e2
Wesamkadah <wesam.kadah@gmail.com>
2023-09-04 22:52:42 +0300

    Initial Commit

---
+++ ummraty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ummraty
+//
+//  Created by Wessam Kadah on 19/02/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
